0b8d3c4d6683c0c39d7d9c89157153b9b02074bf
oliwerin <oliwer.iwanicki@gmail.com>
2016-11-13 19:57:40 +0100

    Got rid of the init function

--- index.js
+++ index.js
@@ -3,27 +3,31 @@
 const through = require('through2');
 const Pusher = require('aemsync').Pusher;
 
-class gulpAemSync {
-    constructor(options) {
-        options = options || {};
+function gulpAemSync(options) {
+    options = options || {};
 
+    let pusher;
+
+    if (!pusher) {
         let targets = options.targets || 'http://admin:admin@localhost:4502';
         let pushInterval = 300;
         let exclude = '';
 
-        this.pusher = new Pusher(targets.split(','), pushInterval, exclude);
+        pusher = new Pusher(targets.split(','), pushInterval, exclude);
     }
 
-    push() {
-        return through.obj((file, encoding, callback) => {
-            console.log(`gulp-aemsync pushing: ${file.path}`);
-
-            this.pusher.enqueue(file.path);
-            this.pusher.processQueue();
-
-            return callback(null, file);
-        });
+    function addFile(file, encoding, callback) {
+        console.log(`gulp-aemsync pushing: ${file.path}`);
+        pusher.enqueue(file.path);
+        callback(null, file);
     }
+
+    function sendPackage(callback) {
+        pusher.processQueue();
+        callback();
+    }
+
+    return through.obj(addFile, sendPackage);
 }
 
 module.exports = gulpAemSync;
